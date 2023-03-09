788f6f75207e1ce23ca4e4c42ec3137d9135aaf2
kztera <nguyenkhoa376@duck.com>
2023-03-10 01:09:01 +0700

    create file script.js with all code in file md

---
+++ I-javaScript-fundamentals/code/script.js
@@ -0,0 +1,140 @@
+// 2. Hello world
+console.log("Hello world");
+
+// 3. Variables and data types
+
+// 3.2. Variables
+/*
+var a = 1; // var is the old way to declare a variable
+let b = 2; // let is the new way to declare a variable (from ES6)
+const c = 3; // const is used to declare a constant variable
+*/
+
+/*
+var a = 1;
+var a = 2;
+console.log(a); // 2
+
+let b = 1;
+let b = 2; // SyntaxError: Identifier 'b' has already been declared
+
+const c = 1;
+c = 2; // TypeError: Assignment to constant variable.
+*/
+
+// 3.2.1. Variable scope
+
+/*
+var a = 1; // global variable
+
+function test() {
+    var b = 2; // local variable
+    console.log(a); // 1
+    console.log(b); // 2
+}
+
+test();
+console.log(a); // 1
+console.log(b); // ReferenceError: b is not defined
+*/
+
+/* 
+var i = 1;
+for (var i = 0; i < 5; i++) {
+    console.log(i); // 0 1 2 3 4
+}
+console.log(i); // 5
+
+let j = 1;
+for (let j = 0; j < 5; j++) {
+    console.log(j); // 0 1 2 3 4
+}
+console.log(j); // 1
+*/
+
+/*
+console.log(a); // undefined (not an error)
+var a = 1;
+
+console.log(b); // ReferenceError: b is not defined
+let b = 1;
+*/
+
+// 3.2.4. typeof operator
+
+var a = 1;
+var b = "Hello World!";
+var c = true;
+
+console.log(typeof a); // number
+console.log(typeof b); // string
+console.log(typeof c); // boolean
+
+var a = null;
+var b = NaN;
+var c = {};
+
+console.log(typeof a); // object
+console.log(typeof b); // number
+console.log(typeof c); // object
+
+
+// 3.2.5. Type Conversion
+
+
+var a = 1; // number
+var b = "Hello World!"; // string
+var c = true; // boolean
+
+a = b; // a is now a string
+c = 3; // c is now a number
+
+var a = 1;
+var b = "2";
+
+console.log(a + b); // 12
+console.log(a + Number(b)); // 3
+
+
+// 3.2.6. Number
+var a = 123;
+var b = 123.456;
+var c = "Hello World!";
+
+var result1 = a + b; // 246.456
+var result2 = a + c; // 123Hello World!
+
+var result3 = (a / c); // NaN
+
+console.log(typeof result3); // number
+
+// 3.2.8. Object
+
+var person = {
+   name: "John",
+   age: 30,
+   city: "New York"
+ };
+
+ 
+console.log(person.name); // John
+
+person.name = "Jane";
+
+console.log(person["name"]); // Jane
+
+// 3.2.8.1 Array
+var fruits = ["Apple", "Banana", "Orange"];
+
+console.log(typeof fruits); // object
+console.log(fruits) 
+/* ["Apple", "Banana", "Orange"]
+   0: "Apple"
+   1: "Banana"
+   2: "Orange"
+   length: 3
+   __proto__: Array(0)
+*/
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Banana
+console.log(fruits[2]); // Orange
